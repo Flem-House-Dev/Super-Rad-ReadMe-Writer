e342d826936b3bc335ac43512f98571de2282387
Jeremy Fleming <thevisionmachine@gmail.com>
2024-04-30 07:35:01 -0500

    created function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,21 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
 
+  ## Installaion
+  ${data.installation}
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
 `;
 }
 
